402a2573745a0becd3c0a63c7099351b16ae4f11
Zinoka <alex.abruzzese96@gmail.com>
2024-01-29 12:59:58 +0100

    remove copy label

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ function createWindow() {
           role: "cut",
         },
         {
-          label: "Copier / Copy",
+          label: "Copier",
           role: "copy",
         },
         {
